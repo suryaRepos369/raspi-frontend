217f7a113756e1a27db5366cdd94719c2ecc117d
suryaRepos369 <105192505+suryaRepos369@users.noreply.github.com>
2022-10-18 18:53:08 +0530

    ui importovement

--- src/components/Controlpage.js
+++ src/components/Controlpage.js
@@ -101,132 +101,57 @@ const Controlpage = () => {
         </div>
       ) : null}
       {/* </div> */}
-      <div className=" container-fluid">
-        <div className="row d-flex">
-          <div className="shadow-md  d-flex flex-grow col-lg-5 col-12 ">
-            <div className="card h-fit flex-grow bg-inherit border-blue-700 m-2 p-1">
-              <div className="card-body p-0  h-fit">
-                <p className="card-title font-monospace text-xl  my-4 p-2 bg-gray-900 text-yellow-400 font-semibold rounded-xl">
-                  Poultry farm Light control board
-                </p>
-
-                <img
-                  src="https://5.imimg.com/data5/SELLER/Default/2021/3/PL/WY/NT/122114398/automatic-poultry-farm-500x500.png"
-                  alt=""
-                  className="h-[100px] w-5/6 m-auto rounded-xl "
-                />
-
-                <div className="card-text min-h-[500px]m-2 rounded-lg my-1 bg-inherit">
-                  <div className="flex gap-8 mt-8 flex-wrap justify-center flex-grow">
-                    {/* Farm 1 control */}
-                    <div className="bg-black flex-grow-1 rounded-3xl  h-fit w-44 ">
-                      {/* <p className="bg-gradient-to-r from-gray-400 to-black lead text-white">Farm 1</p> */}
-                      <span className=" text-white font-semibold border-[1px] px-8 border-white rounded-lg bg-orange-500  ">Farm 1</span>
-                      <div className="flex flex-col  justify-evenly flex-wrap-0 p-0 my-2">
-                        {/*  BUlb 1 */}
-                        <div
-                          className={
-                            bulb1State
-                              ? "flex  m-0 p-0 border-2 border-black  flex-grow rounded-3xl bg-gradient-to-r from-yellow-400 to-yellow-600"
-                              : "flex  m-0 p-0 border-2 border-black flex-grow  rounded-3xl bg-gradient-to-r from-gray-600 to-black"
-                          }>
-                          <img
-                            src={bulb}
-                            alt=""
-                            className="h-[90px] my-auto mx-auto"
-                            onClick={() => {
-                              // setBulb1state((prev) => !prev);
-                              if (!bulb1State) bulbOn(1);
-                              if (bulb1State) bulbOff(1);
-                            }}
-                          />
-                          <div className="m-0 p-0 h-fit bg-white rounded-xl ">
-                            <button
-                              disabled={error}
-                              className={
-                                bulb1State
-                                  ? "bg-red-500 text-sm m-1 rounded-xl px-2 min-h-[30px] min-w-[40px]"
-                                  : "bg-green-500 text-sm m-1 rounded-xl px-2 min-h-[30px] min-w-[40px]"
-                              }
-                              onClick={() => {
-                                // setBulb1state((prev) => !prev);
-                                if (!bulb1State) bulbOn(1);
-                                if (bulb1State) bulbOff(1);
-                              }}>
-                              <p className="text-sm font-extrabold"> {bulb1State ? "Off" : "On"}</p>
-                            </button>
-                          </div>
-                        </div>
-                        {/* BUlb 2 */}
-                        <div
-                          className={
-                            bulb2State
-                              ? "flex  m-0 p-0 border-2 border-black  flex-grow rounded-3xl bg-gradient-to-r from-yellow-400 to-yellow-600"
-                              : "flex  m-0 p-0 border-2 border-black flex-grow  rounded-3xl bg-gradient-to-r from-gray-600 to-black"
-                          }>
-                          <img
-                            src={bulb}
-                            alt=""
-                            className="h-[90px] my-auto mx-auto"
-                            onClick={() => {
-                              setBulb2state((prev) => !prev);
-                              if (!bulb2State) bulbOn(2);
-                              if (bulb2State) bulbOff(2);
-                            }}
-                          />
-                          <div className="m-0 p-0 h-fit bg-white rounded-xl ">
-                            <button
-                              disabled={error}
-                              className={
-                                bulb2State
-                                  ? "bg-red-500 text-sm m-1 rounded-xl px-2 min-h-[30px] min-w-[40px]"
-                                  : "bg-green-500 text-sm m-1 rounded-3xl px-2 min-h-[30px] min-w-[40px]"
-                              }
-                              onClick={() => {
-                                setBulb2state((prev) => !prev);
-                                if (!bulb2State) bulbOn(2);
-                                if (bulb2State) bulbOff(2);
-                              }}>
-                              <p className="text-sm font-extrabold"> {bulb2State ? "Off" : "On"}</p>
-                            </button>
-                          </div>
-                        </div>{" "}
-                        {/* BUlb 3 */}
-                        <div
-                          className={
-                            bulb3State
-                              ? "flex  m-0 p-0 border-2 border-black  flex-grow rounded-3xl bg-gradient-to-r from-yellow-400 to-yellow-600"
-                              : "flex  m-0 p-0 border-2 border-black flex-grow  rounded-3xl bg-gradient-to-r from-gray-600 to-black"
-                          }>
-                          <img
-                            src={bulb}
-                            alt=""
-                            className="h-[90px] my-auto mx-auto"
-                            onClick={() => {
-                              setBulb3state((prev) => !prev);
-                              if (!bulb3State) bulbOn(3);
-                              if (bulb3State) bulbOff(3);
-                            }}
-                          />
-                          <div className="m-0 p-0 h-fit bg-white rounded-xl ">
-                            <button
-                              disabled={error}
-                              className={
-                                bulb3State
-                                  ? "bg-red-500 text-sm m-1 rounded-xl px-2 min-h-[30px] min-w-[40px]"
-                                  : "bg-green-500 text-sm m-1 rounded-xl px-2 min-h-[30px] min-w-[40px]"
-                              }
-                              onClick={() => {
-                                setBulb3state((prev) => !prev);
-                                if (!bulb3State) bulbOn(3);
-                                if (bulb3State) bulbOff(3);
-                              }}>
-                              <p className="text-sm font-extrabold"> {bulb3State ? "Off" : "On"}</p>
-                            </button>
-                          </div>
-                        </div>
-                      </div>{" "}
+      <div className=" row m-6  lg:w-3/4 lg:m-auto lg:my-8 flex container-fluid bg-gradient-to-tr from-black to-blue-400 rounded-xl ">
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid
+            container
+            spacing={2}>
+            <Grid
+              xs={12}
+              md={8}
+              lg={4}>
+              <Item>xs=4</Item>
+            </Grid>
+            <Grid xs={4}>
+              <Item>xs=4</Item>
+            </Grid>
+              </Box>
+            </div>
+          </div>
+          <div class="col col-lg-6">
+            <div className="  m-6  min-h-[250px] lg:my-8 lg:m-auto flex container-fluid bg-gradient-to-tr from-black to-gray-400  rounded-xl my-4">
+              <Box sx={{ flexGrow: 1 }}>
+                <Grid
+                  container
+                  spacing={2}>
+                  <Grid
+                    className="m-auto my-4 h-[150px]"
+                    xs={12}
+                    md={11}>
+                    <div className="  bg-white h-[100px] rounded-xl w-3/5 mx-auto">
+                      <img
+                        className="h-[150px] w-full rounded-xl"
+                        src="https://5.imimg.com/data5/CT/KM/MY-3160562/borewell-air-compressor-pump-500x500.jpg"
+                        alt=""
+                      />
                     </div>
+                  </Grid>
+            <Grid xs={8}>
+              <Item>xs=8</Item>
+            </Grid>
+          </Grid>
+        </Box>
+      </div>
+      <div className=" row  lg:w-3/4 my-8 m-auto flex container-fluid bg-gradient-to-tr from-indigo-400 to-yellow-100 m-2 rounded-xl my-4">
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid
+            container
+            spacing={2}>
+            <Grid
+              className="m-auto my-4 h-[150px]"
+              xs={12}
+              md={11}>
+              <div className="bg-gray-300 h-[100px] rounded-xl w-3/5 mx-auto"></div>
 
                     {/* Farm 2 control */}
 
@@ -302,15 +227,17 @@ const Controlpage = () => {
                               <p className="text-sm font-extrabold"> {bulb4State ? "Off" : "On"}</p>
                             </button>
                           </div>
-                        </div>{" "}
-                        {/* BUlb 4 */}
-                        <div
-                          className={
-                            bulb4State
-                              ? "flex  m-0 p-0 border-2 border-black  flex-grow rounded-3xl bg-gradient-to-r from-yellow-400 to-yellow-600"
-                              : "flex  m-0 p-0 border-2 border-black flex-grow  rounded-3xl bg-gradient-to-r from-gray-600 to-black"
-                          }>
-                          <img
+        <div className=" row m-6   lg:w-3/4 lg:my-8 lg:m-auto flex container-fluid bg-gradient-to-tr from-black to-gray-400  rounded-xl my-4">
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid
+              container
+              spacing={2}>
+              <Grid
+                className="m-auto my-4 h-[150px]"
+                xs={12}
+                md={11}>
+                <div className="  bg-white h-[100px] rounded-xl w-3/5 mx-auto">
+                  <img
                             src={bulb}
                             alt=""
                             className="h-[90px] my-auto mx-auto"
